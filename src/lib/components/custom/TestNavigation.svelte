9321add9cbd0c044916ffe7c05333c35916115bf
Baptiste REQUET <baptiste.requet@ext.biomerieux.com>
2024-10-07 11:39:21 +0200

    minor changes

--- src/lib/components/custom/TestNavigation.svelte
+++ src/lib/components/custom/TestNavigation.svelte
@@ -1,63 +1,63 @@
 <script lang="ts">
-    import { Button } from "$lib/components/ui/button";
-    import { goToNextTest, goToPreviousTest } from "$lib/robot/navigation";
-    import type { RobotTest } from "$lib/robot/types";
-    import { ArrowLeft, ArrowRight } from "lucide-svelte";
-    import KeyboardNavigation from "./KeyboardNavigation.svelte";
+  import { Button } from "$lib/components/ui/button";
+  import { goToNextTest, goToPreviousTest } from "$lib/robot/navigation";
+  import type { RobotTest } from "$lib/robot/types";
+  import { ArrowLeft, ArrowRight } from "lucide-svelte";
+  import KeyboardNavigation from "./KeyboardNavigation.svelte";
 
-    let {
-        failedTests,
-        currentTestId,
-        currentTestIndex,
-        totalFailedTestCount,
-        isPageLoaded,
-    }: {
-        failedTests: RobotTest[];
-        currentTestId: string;
-        currentTestIndex: number;
-        totalFailedTestCount: number | undefined;
-        isPageLoaded: boolean;
-    } = $props();
+  let {
+    failedTests,
+    currentTestId,
+    currentTestIndex,
+    totalFailedTestCount,
+    isPageLoaded,
+  }: {
+    failedTests: RobotTest[];
+    currentTestId: string;
+    currentTestIndex: number;
+    totalFailedTestCount: number | undefined;
+    isPageLoaded: boolean;
+  } = $props();
 
-    let isPreviousTestAvailable = $derived(currentTestIndex > 0);
-    let isNextTestAvailable = $derived(
-        totalFailedTestCount && currentTestIndex < totalFailedTestCount - 1,
-    );
+  let isPreviousTestAvailable = $derived(currentTestIndex > 0);
+  let isNextTestAvailable = $derived(
+    totalFailedTestCount && currentTestIndex < totalFailedTestCount - 1
+  );
 </script>
 
 <KeyboardNavigation
-    handleLeft={() => goToPreviousTest(currentTestId)}
-    handleRight={() => goToNextTest(currentTestId)}
+  handleLeft={() => goToPreviousTest(currentTestId)}
+  handleRight={() => goToNextTest(currentTestId)}
 />
 
 <div class="flex gap-2">
-    <Button
-        on:click={() => goToPreviousTest(currentTestId)}
-        disabled={!isPreviousTestAvailable}
-        variant="outline"
-        size="icon"
-    >
-        <ArrowLeft class="h-4 w-4" />
-    </Button>
-    <Button
-        on:click={() => goToNextTest(currentTestId)}
-        disabled={!isNextTestAvailable}
-        variant="outline"
-        size="icon"
-    >
-        <ArrowRight class="h-4 w-4" />
-    </Button>
+  <Button
+    on:click={() => goToPreviousTest(currentTestId)}
+    disabled={!isPreviousTestAvailable}
+    variant="outline"
+    size="icon"
+  >
+    <ArrowLeft class="h-4 w-4" />
+  </Button>
+  <Button
+    on:click={() => goToNextTest(currentTestId)}
+    disabled={!isNextTestAvailable}
+    variant="outline"
+    size="icon"
+  >
+    <ArrowRight class="h-4 w-4" />
+  </Button>
 </div>
 <span
-    class="whitespace-nowrap overflow-hidden text-ellipsis flex-grow text-left pl-4"
+  class="whitespace-nowrap overflow-hidden text-ellipsis flex-grow text-left pl-4"
 >
-    {failedTests[currentTestIndex]?.name}
-    <span class="font-bold">
-        ({currentTestIndex + 1} / {failedTests.length})
-    </span>
-    <span>
-        {#if !isPageLoaded}
-            (Loading..)
-        {/if}
-    </span>
+  <span class="font-bold mr-1">
+    [{currentTestIndex + 1} / {failedTests.length}]
+  </span>
+  {failedTests[currentTestIndex]?.name}
+  <span>
+    {#if !isPageLoaded}
+      (Loading..)
+    {/if}
+  </span>
 </span>
