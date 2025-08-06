922d0e669b9c2c58be5d8e7f36a9098dee6b8a80
brequet <requet.baptiste@gmail.com>
2025-08-06 20:28:09 +0200

    readme

--- src/lib/components/robot-footer/failed-tests-dialog.svelte
+++ src/lib/components/robot-footer/failed-tests-dialog.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
+  import { CodeBlock } from "$lib/components/shared/code-block";
   import { buttonVariants } from "$lib/components/ui/button/index.js";
   import * as Dialog from "$lib/components/ui/dialog";
-  import { formatTestsAsRobotParams } from "$lib/features/failed-test-reporter/failed-tests-reporter.service";
-  import { getContext } from "svelte";
-  import { CodeBlock } from "$lib/components/shared/code-block";
   import { CONTEXT_KEY_DIALOG_CONTAINER } from "$lib/core/constants";
-  import type { RobotTest } from "$lib/features/robot-log/robot.types";
   import type { CodeSnippet } from "$lib/features/curl-generator/curl-generator.types";
+  import { formatTestsAsRobotParams } from "$lib/features/failed-test-reporter/failed-tests-reporter.service";
+  import type { RobotTest } from "$lib/features/robot-log/robot.types";
+  import { getContext } from "svelte";
 
   type Props = {
     failedTests: RobotTest[];
@@ -18,7 +18,7 @@
     CONTEXT_KEY_DIALOG_CONTAINER,
   );
 
-  const failingTestsSnippets: CodeSnippet[] = $derived.by(() => {
+  const failedTestsSnippets: CodeSnippet[] = $derived.by(() => {
     const params = formatTestsAsRobotParams(failedTests);
     return [{ fileType: "Shell", text: params }];
   });
@@ -26,19 +26,19 @@
 
 <Dialog.Root>
   <Dialog.Trigger class={buttonVariants({ variant: "default" })}>
-    Print all failing tests as robot test params in console
+    Print all failed tests as robot test params in console
   </Dialog.Trigger>
   <Dialog.Content
     class="flex h-full max-h-[80vh] flex-col sm:max-w-4xl"
     portalProps={{ to: dialogContainer() }}
   >
     <Dialog.Header>
-      <Dialog.Title>Failing tests as Robot params</Dialog.Title>
+      <Dialog.Title>Failed tests as Robot params</Dialog.Title>
       <Dialog.Description>
-        You can copy and use these params to relaunch only failing tests.
+        You can copy and use these params to relaunch only failed tests.
       </Dialog.Description>
     </Dialog.Header>
 
-    <CodeBlock snippets={failingTestsSnippets} />
+    <CodeBlock snippets={failedTestsSnippets} />
   </Dialog.Content>
 </Dialog.Root>
